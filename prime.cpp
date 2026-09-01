91725c18149f5fa714851f6b991c25e134dea1f7
parth <bcasy2655@gmail.com>
2026-09-01 11:07:45 -0700

    palindrome

--- prime.cpp
+++ prime.cpp
@@ -1,24 +1,18 @@
 #include<iostream>
 using namespace std;
-
 bool isPrime(int n, int i) {
     if(n <= 2)
         return (n == 2);
-
     if(n % i == 0)
         return false;
-
     if(i * i > n)
         return true;
-
     return isPrime(n, i + 1);
 }
-
 int main() {
     int n;
     cout << "enter a number:";
     cin >> n;
-
     if(isPrime(n, 2))
         cout << "prime number";
     else
